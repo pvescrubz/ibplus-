2db06835bb14f4316eaf77deaa8316340e57d165
qseven <qseven2012@gmail.com>
2024-11-11 18:44:39 +0300

    new page

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,16 @@
 // Открытие окна настроек при клике на кнопку
+// Открытие окна настроек при клике на кнопку
 document.querySelectorAll('[data-btn="settings_btn"]').forEach(button => {
     button.addEventListener('click', function(event) {
       event.stopPropagation(); // Останавливаем всплытие, чтобы избежать немедленного закрытия
       document.querySelector('.settings_content').style.display = 'flex';
     });
   });
-
+  
   // Закрытие окна при клике вне его области
-document.addEventListener('click', function(event) {
+  document.addEventListener('click', function(event) {
     const settingsContent = document.querySelector('.settings_content');
+  
     // Проверяем, открыт ли попап и что клик не внутри окна настроек
     if (
       settingsContent.style.display === 'flex' &&
@@ -18,6 +20,16 @@ document.addEventListener('click', function(event) {
       settingsContent.style.display = 'none';
     }
   });
+  
+  // Закрытие окна при начале скролла
+  window.addEventListener('scroll', function() {
+    const settingsContent = document.querySelector('.settings_content');
+  
+    if (settingsContent.style.display === 'flex') {
+      settingsContent.style.display = 'none';
+    }
+  });
+  
   
 
 
